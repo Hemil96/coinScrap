63320cfddc97be18fbea34ffb19958525dba5402
Hemil <hemilpatel41196@gmail.com>
2021-02-27 09:44:39 +1100

    working

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const axios = require('axios');
+const { response } = require('express');
+const app = express()
+const port = 3001
+
+app.get('/', async (req, res) => {
+  
+  const url = 'https://api.wazirx.com/api/v2/tickers/dogeusdt';
+  let data = ''
+
+  await axios.get(url)
+    .then(response => {
+      data = response.data
+      console.log(data)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    return res.status(200).send(data)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
